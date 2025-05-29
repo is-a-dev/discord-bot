2a39fe5c16fd48760f6d52d852f3132c6b635f49
William Harrison <87287585+wdhdev@users.noreply.github.com>
2025-05-29 18:21:30 +0800

    Create available-single-letters.ts

---
+++ src/commands/is-a-dev/available-single-letters.ts
@@ -0,0 +1,55 @@
+import Command from "../../classes/Command";
+import ExtendedClient from "../../classes/ExtendedClient";
+import { ChatInputCommandInteraction, ColorResolvable } from "discord.js";
+
+import axios from "axios";
+import { emojis as emoji } from "../../../config.json";
+
+const command: Command = {
+    name: "available-single-letters",
+    description: "Check if there are any available single letter subdomains.",
+    options: [],
+    botPermissions: [],
+    requiredRoles: [],
+    cooldown: 5,
+    enabled: true,
+    deferReply: true,
+    ephemeral: false,
+    async execute(
+        interaction: ChatInputCommandInteraction,
+        client: ExtendedClient,
+        Discord: typeof import("discord.js")
+    ) {
+        try {
+            const subdomains = "abcdefghijklmnopqrstuvwxyz0123456789".split("");
+
+            const res = (await axios.get("https://raw.is-a.dev/v2.json")).data;
+
+            const taken = res.map((entry: any) => entry.subdomain);
+            const available = subdomains.filter((subdomain) => !taken.includes(subdomain));
+
+            if (!available.length) {
+                const noAvailable = new Discord.EmbedBuilder()
+                    .setColor(client.config.embeds.error as ColorResolvable)
+                    .setDescription(`${emoji.cross} No single letter subdomains are available.`);
+
+                await interaction.editReply({ embeds: [noAvailable] });
+                return;
+            }
+            
+            const availableList = available.map((subdomain) => `- \`${subdomain}.is-a.dev\``).join("\n");
+
+            const availableEmbed = new Discord.EmbedBuilder()
+                .setColor(client.config.embeds.default as ColorResolvable)
+                .setTitle("Available Single Letter Subdomains")
+                .setDescription(availableList)
+                .setTimestamp()
+
+            await interaction.editReply({ embeds: [availableEmbed] });
+        } catch (err) {
+            client.logCommandError(err, interaction, Discord);
+        }
+    }
+};
+
+export = command;
