a752fd3d036cd4a809ebdfc6963bc86f355e3869
William Harrison <87287585+wdhdev@users.noreply.github.com>
2025-06-02 20:45:22 +0800

    Update interactionCreate.ts

--- src/events/guild/interactionCreate.ts
+++ src/events/guild/interactionCreate.ts
@@ -1,18 +1,22 @@
-import Event from "../../classes/Event";
+import { GuildEvent } from "../../classes/Event";
 import ExtendedClient from "../../classes/ExtendedClient";
-import { Interaction } from "discord.js";
+import { Interaction, PermissionResolvable } from "discord.js";
 
-// import autocompleteHandler from "../../util/interaction/autocomplete";
-// import commandHandler from "../../util/interaction/command";
+import autocompleteHandler from "../../util/interaction/autocomplete";
+import commandHandler from "../../util/interaction/command";
 
-const event: Event = {
-    name: "interactionCreate",
+const event: GuildEvent = {
+    name: "InteractionCreate",
     once: false,
     async execute(client: ExtendedClient, Discord: typeof import("discord.js"), interaction: Interaction) {
         try {
-            // if (!interaction.guild || interaction.guild.id !== client.config.guild) return;
-            // if (interaction.isAutocomplete()) return await autocompleteHandler(client, interaction);
-            // if (interaction.isChatInputCommand()) return await commandHandler(client, Discord, interaction);
+            const requiredPerms: PermissionResolvable = ["SendMessages", "EmbedLinks"];
+
+            if (!interaction.guild || interaction.guild.id !== client.config.guild) return;
+            if (!interaction.guild.members.me.permissions.has(requiredPerms)) return;
+
+            if (interaction.isAutocomplete()) return await autocompleteHandler(client, interaction);
+            if (interaction.isChatInputCommand()) return await commandHandler(client, Discord, interaction);
         } catch (err) {
             client.logError(err);
         }
